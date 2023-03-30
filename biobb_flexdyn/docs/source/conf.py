2dba47fe3c7722592b86c19e205ba46129e139ad
Adam Hospital Gasch <hospital@OROZCO67.local>
2023-03-30 10:55:50 +0200

    prody rtd

--- biobb_flexdyn/docs/source/conf.py
+++ biobb_flexdyn/docs/source/conf.py
@@ -19,6 +19,7 @@ from pathlib import Path
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, str(Path('../../').resolve()))
+autodoc_mock_imports = ["prody"]
 
 # -- General configuration ------------------------------------------------
 
