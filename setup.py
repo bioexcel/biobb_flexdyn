5a9e970f1fb020b32bae16e0bef137896bebb33d
gbayarri <genis.bayarri@irbbarcelona.org>
2026-03-05 13:38:08 +0100

    2025.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_flexdyn",
-    version="5.2.0",
+    version="5.2.1",
     author="Biobb developers",
     author_email="adam.hospital@irbbarcelona.org",
     description="biobb_flexdyn is a BioBB category for studies on the conformational landscape of native proteins.",
@@ -20,7 +20,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=['docs', 'test']),
     package_data={'biobb_flexdyn': ['py.typed']},
     include_package_data=True,
-    install_requires=['biobb_common==5.2.0', 'scipy>=1.13.0,<1.14.0'],
+    install_requires=['biobb_common==5.2.2', 'scipy>=1.13.0,<1.14.0'],
     python_requires='>=3.10,<3.12',
     entry_points={
         "console_scripts": [
